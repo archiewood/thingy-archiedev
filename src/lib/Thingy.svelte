613d95c61f08b78fe39bfe1aafc5cf15b381d2ec
Archie Wood <58074498+archiewood@users.noreply.github.com>
2023-05-30 15:26:37 -0400

    initial commit

---
+++ src/lib/Thingy.svelte
@@ -0,0 +1,12 @@
+<div>You've got thingy!</div>
+
+
+<style>
+    div {
+        color: blue;
+        font-size: xx-large;
+        background-color: #eee;
+        border: 1px solid #ccc;
+        padding: 1em;
+    }
+</style>
